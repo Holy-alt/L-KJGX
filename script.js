d131287e49131b1da1c2597818da5f4e1028f819
Holy-alt <dimadonisev@gmail.com>
2025-02-14 20:22:53 +0200

    SDFGHJKL

--- script.js
+++ script.js
@@ -17,23 +17,3 @@ document.addEventListener('DOMContentLoaded', () => {
    
 });
 
-more_btn = document.querySelectorAll('.more');
-console.log('more_btn', more_btn)
-container = document.querySelectorAll('.class_toogle');
-cancel = document.querySelectorAll('.cancel');
-
-for (let i = 0; i < more_btn.length; i++) {
-    more_btn[i].addEventListener('click', function () {
-        console.log('clicked', i)
-        container[i].classList.toggle('flip-card');
-    })
-}
-
-
-for (let i = 0; i < cancel.length; i++) {
-    cancel[i].addEventListener('click', function () {
-        console.log('clicked', i)
-        container[i].classList.toggle('flip-card');
-    })
-}
-
